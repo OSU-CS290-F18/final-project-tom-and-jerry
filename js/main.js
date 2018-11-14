9eaa1d299376f6acff5e11b3cc43b87de3c391ca
xns1997 <723648649@qq.com>
2018-11-14 15:06:20 -0800

    new version

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var test =document.getElementsByClassName("post");
+var testbutton=document.getElementsByTagName("button");
+testbutton[0].onclick=function(){
+	
+};
+//test[0].style.height="700px";
+//test[0].getElementsByClassName("mediabox")[0].style.height="450px";
+//test[0].getElementsByClassName("infobox")[0].style.height="250px";
+myChart.data.datasets["0"].data[0]="50"
