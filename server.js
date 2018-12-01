d04eb4e4843d617a42cebfd54b88dc776640ec8e
Ying <liyi4@oregonstate.edu>
2018-12-01 15:41:07 -0800

    added mongodb

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var bodyParser = require('body-parser');
 var voteData = require('./voteData');
 
 var MongoClient = require('mongodb').MongoClient;
-var mongoHost = process.env.MONGO_HOST;
+var mongoHost = process.env.MONGO_HOST || 'classmongo.engr.oregonstate.edu';
 var mongoPort = process.env.MONGO_PORT || '27017';
-var mongoUsername = process.env.MONGO_USERNAME;
-var mongoPassword = process.env.MONGO_PASSWORD;
-var mongoDBName = process.env.MONGO_DB_NAME;
+var mongoUsername = process.env.MONGO_USERNAME || 'cs290_liyi4';
+var mongoPassword = process.env.MONGO_PASSWORD || 'cs290_liyi4';
+var mongoDBName = process.env.MONGO_DB_NAME || 'cs290_liyi4';
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -31,19 +31,44 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
-  console.log("== 200");
+  var voteCollection = mongoDB.collection('vote');
+  //console.log('==',voteCollection);
+  voteCollection.find({ "id":"bar001" }).toArray(function (err, voteDocs) {
+    if (err) {
+      res.status(500).send("Error communicating with the DB.");
+    } else if (voteDocs.length > 0) {
+      console.log('== vote: /n', voteDocs[0]);
+    } else {
+      next();
+    }
+  });
+  console.log("== homepage ==");
   res.status(200).render('homePage',{vote:voteData});
 });
+
 //app.get('/index', function (req, res, next) {
 //    console.log("== 200");
 //    res.status(200).render('homePage');
 //});
 
-app.listen(port, function () {
-  console.log("== Server listening on port", port);
-})
-
 app.get('*', function (req, res, next) {
-  console.log("== 404");
+  console.log("== 404 ==");
   res.status(404).render('404');
+});
+
+
+console.log("== connecting to ",mongoURL,".......");
+MongoClient.connect(mongoURL,{ useNewUrlParser: true },function (err, client) {
+  if (err) {
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+  console.log("== connected success to ",mongoURL);
+  app.listen(port, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("== Server listening on port", port);
+  })
+  
 });
