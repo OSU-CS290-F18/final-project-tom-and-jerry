55c935fffc911e8a821bac6fe1d616d7fda8dfaf
Ying <liyi4@oregonstate.edu>
2018-12-01 11:38:33 -0800

    update md

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # final-project-tom-and-jerry
-Team Tom_And_Jerry<br>
-Team Members:<br> 
-              Naisen Xu <br>
-              Ying Li<br>
-              Yujie Wang<br>
+###Vote Page
+Team Tom_And_Jerry
+**Team Members:**
+              *Naisen Xu*
+              *Ying Li*
+              ~~Yujie Wang~~
 
+[Any questions?](xuna@oregonstate.edu) 
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-var path = require('path');
+//var path = require('path');
+//var Handlebars = require("handlebars");
+//var partials = "./views/partials/";
+
+var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var Handlebars = require("handlebars");
-var partials = "./views/partials/";
+
 var voteData = require('./voteData');
+
+var MongoClient = require('mongodb').MongoClient;
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
+
+var mongoDB = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -20,13 +34,15 @@ app.get('/', function (req, res, next) {
   console.log("== 200");
   res.status(200).render('homePage',{vote:voteData});
 });
-app.get('/index', function (req, res, next) {
-    console.log("== 200");
-    res.status(200).render('homePage');
-  });
+//app.get('/index', function (req, res, next) {
+//    console.log("== 200");
+//    res.status(200).render('homePage');
+//});
+
 app.listen(port, function () {
   console.log("== Server listening on port", port);
 })
+
 app.get('*', function (req, res, next) {
   console.log("== 404");
   res.status(404).render('404');
